de68bebe02521932fc95dde73b21c3f3a4283094
Yvette Waller <vetty88@gmail.com>
2021-05-27 15:47:22 +1000

    reverted many changes back to original fixed cross origin error and react hooks error

--- server/index.js
+++ server/index.js
@@ -1,12 +1,4 @@
-const express = require('express');
-const path = require('path');
-const cluster = require('cluster');
-const numCPUs = require('os').cpus().length;
-
-const isDev = process.env.NODE_ENV !== 'production';
-const PORT = process.env.PORT || 5000;
-
-
+const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
@@ -15,7 +7,6 @@ const horses = require("./routes/api/horses");
 const competitions = require("./routes/api/competitions");
 
 const app = express();
-app.use(cors());
 // Bodyparser middleware
 app.use(
   bodyParser.urlencoded({
@@ -42,37 +33,7 @@ app.use("/api/users", users);
 app.use("/api/horses", horses);
 app.use("/api/competitions", competitions);
 
-// Multi-process to utilize all CPU cores.
-if (!isDev && cluster.isMaster) {
-  console.error(`Node cluster master ${process.pid} is running`);
-
-  // Fork workers.
-  for (let i = 0; i < numCPUs; i++) {
-    cluster.fork();
-  }
-
-  cluster.on('exit', (worker, code, signal) => {
-    console.error(`Node cluster worker ${worker.process.pid} exited: code ${code}, signal ${signal}`);
-  });
-
-} else {
-  const app = express();
-
-  // Priority serve any static files.
-  app.use(express.static(path.resolve(__dirname, '../react-ui/build')));
-
-  // Answer API requests.
-  app.get('/api', function (req, res) {
-    res.set('Content-Type', 'application/json');
-    res.send('{"message":"Hello from the custom server!"}');
-  });
-
-  // All remaining requests return the React app, so it can handle routing.
-  app.get('*', function(request, response) {
-    response.sendFile(path.resolve(__dirname, '../react-ui/build', 'index.html'));
-  });
-
-  app.listen(PORT, function () {
-    console.error(`Node ${isDev ? 'dev server' : 'cluster worker '+process.pid}: listening on port ${PORT}`);
-  });
-}
+const server = app.listen(process.env.PORT || 5000, () => {
+  const port = server.address().port;
+  console.log(`Express is working on port ${port}`);
+});
