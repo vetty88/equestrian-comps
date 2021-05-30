02821e357b581dc9575602dd15dcc65f402c1445
Yvette Waller <vetty88@gmail.com>
2021-05-30 17:51:17 +1000

    updated sentry errors

--- react-ui/src/index.js
+++ react-ui/src/index.js
@@ -1,14 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
+import App from "./App";
 
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+Sentry.init({
+  dsn: "https://203ae0c6c5274db9bf6bfe2ab983cd97@o754816.ingest.sentry.io/5791867",
+  integrations: [new Integrations.BrowserTracing()],
 
-import './index.css';
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<App />, document.getElementById("root"));
