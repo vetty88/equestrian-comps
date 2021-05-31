571b983232e757fb18df72745d25e891d0abdcd3
Yvette Waller <vetty88@gmail.com>
2021-05-31 11:36:15 +1000

    new build

---
+++ server/index.js
@@ -0,0 +1,45 @@
+// HTTP proxy
+// Just replace 'http://' with 'https://' to use HTTPS proxy
+require('friendly-error')({
+  proxy: 'http://localhost:8080'
+})
+
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const passport = require("passport");
+const users = require("./routes/api/users");
+const horses = require("./routes/api/horses");
+const competitions = require("./routes/api/competitions");
+
+const app = express();
+// Bodyparser middleware
+app.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+);
+app.use(bodyParser.json());
+
+// DB Config
+const db = require("./config/keys").mongoURI;
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/equestriancompsdb");
+
+
+// Passport middleware
+app.use(passport.initialize());
+
+// Passport config
+require("./config/passport")(passport);
+
+// Routes
+app.use("/api/users", users);
+app.use("/api/horses", horses);
+app.use("/api/competitions", competitions);
+
+const server = app.listen(process.env.PORT || 8080, () => {
+  const port = server.address().port;
+  console.log(`Express is working on port ${port}`);
+});
