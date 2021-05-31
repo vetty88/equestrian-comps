deac8a03544a2ed7d340c1599f4e63891b80b456
Yvette Waller <vetty88@gmail.com>
2021-05-31 16:08:06 +1000

    update build

---
+++ server/config/config.js
@@ -0,0 +1,8 @@
+//This file holds any configuration variables we may need
+//'config.js' is usually ignored by git to protect sensitive information, such as your database's username and password
+
+module.exports = {
+    db: {
+        uri: 'mongodb+srv://vetty88:fXP1EMS6Ww0nANsY@cluster0.0cv9z.gcp.mongodb.net/equestriancompsdb?authSource=admin', //place the URI of your mongo database here.
+    }
+};
--- server/config/express.js
+++ server/config/express.js
@@ -11,7 +11,7 @@ module.exports.init = () => {
         connect to database
         - reference README for db uri
     */
-    mongoose.connect(process.env.DB_URI || require('../config/config.js').db.uri, {
+    mongoose.connect(process.env.DB_URI || require("../config/config").db.uri, {
         useNewUrlParser: true
     });
     mongoose.set('useCreateIndex', true);
